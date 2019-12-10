d845d6868c31481da63a80c0df894e4615a7722f
Pro X <pro@proteanx.com>
2019-12-10 10:13:37 -0500

    update rpc port + man/docs

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -34,9 +34,9 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 12311);
+        return MakeUnique<CBaseChainParams>("", 42421);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet1", 12313);
+        return MakeUnique<CBaseChainParams>("testnet1", 42025);
     else if (chain == CBaseChainParams::REGTEST)
         return MakeUnique<CBaseChainParams>("regtest", 29332);
     else
